9f713fd8361938bbf4b8cddd93b18d354fe7f9c6
Sam Roberts <samroberts707@gmail.com>
2021-10-19 15:41:14 +0100

    roll-back nav updates from this branch

--- components/nav.vue
+++ components/nav.vue
@@ -6,32 +6,8 @@
     </nav>
 </template>
 <script>
-import _ from "lodash"
 export default {
     name: 'Navigation',
-    created() {
-        this.$router.options.routes.forEach(route => {
-            // We don't want top level routes such as home, we want to control the order of these.
-            if(route.path.split("/")[1] == 'components') {
-                this.rawRoutes.push({
-                    name: route.name,
-                    path: route.path,
-                    parentGroup: route.path.split("/")[2],
-                })
-            }
-        })
-    },
-    data() {
-        return {
-            rawRoutes: [],
-            groupedRoutes: []
-        }
-    },
-    mounted() {
-        // This is a lodash function -- https://lodash.com/docs/4.17.15#groupBy
-        this.groupedRoutes = _.groupBy(this.rawRoutes, route => route.parentGroup);
-        // console.log(this.groupedRoutes);
-    }
 }
 </script>
 <style lang="scss" scoped>
